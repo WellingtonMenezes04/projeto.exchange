83d26d1711a0a0b0ff2278081b9b2d6903f16cb0
YuriOishi <yurilucasoishi@gmail.com>
2024-10-01 22:39:55 -0300

    removendo arquivos desnecessarios

--- validar_senha.c
+++
@@ -1,7 +0,0 @@
-// função para validar a senha
-int validar_senha() {
-    char senha[12];
-    printf("Digite sua senha: ");
-    scanf("%s", senha);
-    return strcmp(usuario.senha, senha) == 0; // retorna 1 se a senha estiver correta e 0 caso contrario
-}
